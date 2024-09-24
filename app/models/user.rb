aaf40ef9f0401fd2d76b0d3ca6186ab4a03bac3e
Ayaan Shahid <ayaanshahid222@gmail.com>
2024-09-24 16:23:56 +0500

    Added devise(along with name and role column)

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { manager: 0, qa: 1, developer: 2 }
+  validates :role, presence: true, inclusion: { in: roles.keys }
+  validates :name, presence: true
+end
